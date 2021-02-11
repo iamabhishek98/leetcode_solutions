791ebea94c45332e6fe8a60bff1cc8e0811a9384
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-02-11 16:52:21 +0800

    Time: 48 ms (16.89%), Space: 18.8 MB (7.12%) - LeetHub

--- copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -1,3 +1,5 @@
+from copy import deepcopy as cp
+
 """
 # Definition for a Node.
 class Node:
@@ -9,36 +11,4 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        if (head is None): return None
-        elif head.next is None: 
-            ans = Node(head.val,None,None)
-            if (head.random is None): return ans
-            ans.random = ans
-            return ans
-        copy = Node(head.val)
-        ans = copy
-        start = head
-        start = start.next
-        while (start):
-            new = Node(start.val)
-            copy.next = new
-            copy = copy.next
-            start = start.next
-        
-        start = head
-        copy = ans
-        i = 0
-        while(start):
-            temp = start.next
-            start.next = copy
-            copy.random = start
-            start = temp
-            copy = copy.next
-            i+=1
-            
-        copy = ans
-        while (copy):
-            copy.random = copy.random.random.next if copy.random.random is not None else None
-            copy = copy.next
-        
-        return ans
+        return cp(head)

b6c8f9f0152c6fa56c715076d468fcb900d4b5a7
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-04-28 11:12:49 +0800

    Time: 84 ms (30.86%), Space: 13.6 MB (11.59%) - LeetHub

--- power-of-three/power-of-three.py
+++ power-of-three/power-of-three.py
@@ -1,9 +1,14 @@
-class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-        powers = set()
-        powers.add(1)
-        curr = 1
-        for i in range(1291):
-            powers.add(curr*3)
-            curr*=3
-        return n in powers
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        ori = n
+        if (n<=0): return False
+        i = 0
+        while(n>0):
+            n/=3
+            i+=1
+        if (pow(3,i-1)==ori): return True
+        return False

735d832c069d13b3e037015613744f7420987a4e
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-05-21 17:50:32 +0800

    Time: 20 ms (73.95%), Space: 13.6 MB (46.22%) - LeetHub

---
+++ find-and-replace-pattern/find-and-replace-pattern.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findAndReplacePattern(self, words, pattern):
+        """
+        :type words: List[str]
+        :type pattern: str
+        :rtype: List[str]
+        """
+        def check(word):
+            m1, m2 = {}, {}
+            if len(word) != len(pattern): return False
+            for i in range(len(word)):
+                if word[i] not in m1: m1[word[i]] = pattern[i]
+                if pattern[i] not in m2: m2[pattern[i]] = word[i]
+                if (word[i], pattern[i]) != (m2[pattern[i]], m1[word[i]]): return False
+            return True
+        
+        ans = []
+        for word in words:
+            if check(word): ans.append(word)
+        return ans

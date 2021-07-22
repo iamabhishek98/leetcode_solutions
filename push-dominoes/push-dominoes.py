586db47ff59ca04f0d74fd443301b8cfa4046af9
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-07-22 15:14:33 +0800

    Time: 484 ms (17.24%), Space: 21.3 MB (22.41%) - LeetHub

--- push-dominoes/push-dominoes.py
+++ push-dominoes/push-dominoes.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         # https://www.youtube.com/watch?v=Z2x-zalsebs&ab_channel=NickWhite
         
-        forces = [0 for _ in range(len(dominoes))]
+        forces = []
         force = 0
         
         # loop from left to right to account for right force
@@ -19,23 +19,23 @@ class Solution(object):
                 # after each domino is knocked down, the force decrements by one
                 force = max(force-1, 0)
             # since the force is initially 0, this is the net positive force added to it
-            forces[i] += force
+            forces.append(force)
         
-        # loop from right to left to account for left force:
+        # loop from right to left to account for left force
         for i in range(len(dominoes)-1,-1,-1):
             if dominoes[i] == 'L':
                 force = len(dominoes)
             elif dominoes[i] == 'R':
                 force = 0
             else:
-                force = max(force-1,0)
+                force = max(force-1, 0)
             # since the right forces are already in the arr, the left force must be subtracted from it
             forces[i] -= force
 
         ans = ""
         for i in forces:
-            if i > 0: ans+="R"
-            elif i < 0: ans+="L"
+            if i > 0: ans += "R"
+            elif i < 0: ans += "L"
             else: ans+="."
         
         return ans

a5ab193e7621c73f914653b41c0e43e3db75235d
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-07-28 14:26:58 +0800

    Time: 44 ms (59.85%), Space: 13.6 MB (94.65%) - LeetHub

--- string-compression/string-compression.py
+++ string-compression/string-compression.py
@@ -4,29 +4,31 @@ class Solution(object):
         :type chars: List[str]
         :rtype: int
         """
+        # https://www.youtube.com/watch?v=IhJgguNiYYk&ab_channel=KevinNaughtonJr.
+        
+        # approach is to keep overwriting the previous characters
+        # the returned new length of the string is the part of the string tht will be considered for the solution
+        
+        index = 0
         left = 0
-        count = 0
+        
         while left < len(chars):
             right = left + 1
             while right < len(chars) and chars[left] == chars[right]:
                 right += 1
-            curr = right - left - 1
-            if curr == 0: 
-                count += 1
-                left += 1
-            else:
-                temp = curr + 1
-                while curr:
-                    chars.pop(left+1)
-                    curr -= 1
+            
+            # index is incremented because the current char has already been written
+            chars[index] = chars[left]
+            index += 1
+            
+            curr = right - left
+            if curr > 1: 
+                for i in str(curr):
+                    chars[index] = i
+                    index += 1
                     
-                curr_arr = list(str(temp))
-                for i in range(len(curr_arr)-1,-1,-1):
-                    chars.insert(left + 1, curr_arr[i])
-                
-                count += temp
-                left+=2
+            left = right
                 
         
-        return count
+        return index
         

65f9fce85d86cf1b8efaa0b7b4e70ae399316765
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-08-11 11:05:36 +0800

    Time: 244 ms (28.13%), Space: 14.6 MB (53.13%) - LeetHub

--- flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
+++ flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.py
@@ -6,17 +6,24 @@ class Solution(object):
         """
         # https://www.youtube.com/watch?v=Do2BcLMfyoE&ab_channel=CodingDecoded
         
+        # algo:
+        # We loop through the string.
+        # If we got a 1, go on. No need to flip. We just increment the 1 counter.
+        # If we got a 0, we increment the flips counter.
+        # Now, we have two options. Either to flip the new zero to one or to flip all previous ones to zeros.
+        # So we take the min between flips and counter at every step of the way.
+        
         witnessOne = False
-        zeroToOneCount = 0
+        flipZeroToOneCount = 0
         oneCount = 0
         
         for i in s:
-            if i == "1":
-                witnessOne = True
-            if witnessOne:
-                if i == "0": zeroToOneCount += 1
-                else: oneCount += 1
-                    
-                zeroToOneCount = min(zeroToOneCount, oneCount)
+            if i == "0" and not witnessOne: continue
+            witnessOne = True
+            
+            if i == "0": flipZeroToOneCount += 1
+            else: oneCount += 1  
+            
+            flipZeroToOneCount = min(flipZeroToOneCount, oneCount)
         
-        return min(zeroToOneCount, oneCount)
+        return flipZeroToOneCount

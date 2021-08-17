d38cdedc6d8b9a3fde8b8b0961d993d69de928c9
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-08-17 11:03:40 +0800

    Time: 552 ms (64.66%), Space: 23.2 MB (39.47%) - LeetHub

--- range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,16 +1,15 @@
 class NumMatrix(object):
-    # https://www.youtube.com/watch?v=7mL8KJ4Pi70
+
     def __init__(self, matrix):
         """
         :type matrix: List[List[int]]
         """
-        rows = len(matrix)
-        cols = len(matrix[0])
-        self.dp = [[0 for j in range(cols+1)] for i in range(rows+1)]
-        for i in range(rows):
-            for j in range(cols):
-                self.dp[i+1][j+1] = matrix[i][j]+self.dp[i][j+1]+self.dp[i+1][j]-self.dp[i][j]
-
+        self.dp = [[0 for _ in range(len(matrix[0])+1)] for __ in range(len(matrix)+1)]
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                self.dp[i+1][j+1] += matrix[i][j] + self.dp[i][j+1] + self.dp[i+1][j] - self.dp[i][j] 
+        
     def sumRegion(self, row1, col1, row2, col2):
         """
         :type row1: int
@@ -20,7 +19,6 @@ class NumMatrix(object):
         :rtype: int
         """
         return self.dp[row2+1][col2+1]-self.dp[row1][col2+1]-self.dp[row2+1][col1]+self.dp[row1][col1]
-        
 
 
 # Your NumMatrix object will be instantiated and called as such:

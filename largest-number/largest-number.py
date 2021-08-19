ca43e1618eb5d111bbd7f02d8717704218094df0
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-08-19 12:20:35 +0800

    Time: 24 ms (82.52%), Space: 13.6 MB (25.71%) - LeetHub

--- largest-number/largest-number.py
+++ largest-number/largest-number.py
@@ -4,7 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: str
         """
-        
+        # we sort this way instead of descending order because the number can have more than 1 digit in which case, there might be edge cases
+        # e.g. take 30 and 3. descending order gives 303 but we want 330 so we use pairwise comparison for each sort: 303 < 330
         nums = sorted(map(str,nums),cmp=(lambda x,y : 1 if x+y < y+x else -1))
         res = ''.join(nums)
         # the most significant number will be in front so if 0 is in front, we return 0 as we know the result is a string of 0s

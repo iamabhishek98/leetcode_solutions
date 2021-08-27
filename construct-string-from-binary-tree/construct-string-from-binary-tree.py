dafe0c4ad2b0eec81c5b3576297a94d104841213
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-08-27 12:31:03 +0800

    Time: 56 ms (12.95%), Space: 16.2 MB (52.52%) - LeetHub

---
+++ construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+class Solution(object):
+    def tree2str(self, root):
+        """
+        :type root: TreeNode
+        :rtype: str
+        """
+        def dfs(node = root):
+            if node is None: 
+                return ""
+            
+            # no need for parenthesis in this case
+            if not node.left and not node.right:
+                return str(node.val)
+            
+            # can ignore node.right
+            if not node.right:
+                return str(node.val)+"("+dfs(node.left)+")"
+            
+            # need to consider all children
+            return str(node.val)+"("+dfs(node.left)+")"+"("+dfs(node.right)+")"
+        
+        return dfs()

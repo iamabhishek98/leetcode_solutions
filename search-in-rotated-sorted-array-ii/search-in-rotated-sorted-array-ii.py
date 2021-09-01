45fd7a651e788b0af9e7b72a6638ba0ad78ab091
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-09-01 11:09:25 +0800

    Time: 36 ms (76.91%), Space: 14 MB (16.06%) - LeetHub

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -5,14 +5,44 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        if target >= nums[0]:
-            prev = nums[0]
-            for i in range(len(nums)):
-                if prev > nums[i]: return False
-                if nums[i] == target: return True
-        else:
-            prev = nums[-1]
-            for i in range(len(nums)-1,0,-1):
-                if prev < nums[i]: return False
-                if nums[i] == target: return True
-        return False
+        left = 0
+        right = len(nums) - 1
+        
+        if target == nums[0] or target == nums[-1]: return True
+        
+        while left < right:
+            # to remove duplicates
+            while left < right and nums[left] == nums[left+1]: left += 1
+            while left < right and nums[right] == nums[right-1]: right -= 1
+                
+            mid = (left + right)/2
+            
+            if nums[mid] == target: return mid
+            
+            # target might be in rotated part
+            if target > nums[0]:
+                # we are in rotated part
+                if nums[mid] > nums[0]:
+                    if target < nums[mid]:
+                        right = mid
+                    else:
+                        left = mid + 1
+                
+                # we are in unrotated part so we need to go left regardless
+                else:
+                    right = mid
+                    
+            # target might be in unrotated part
+            else:
+                # we are in rotated part so we need to go right regardless
+                if nums[mid] > nums[0]:
+                    left = mid + 1
+                
+                # we are in unrotated part
+                else:
+                    if target < nums[mid]:
+                        right = mid
+                    else:
+                        left = mid + 1
+            
+        return nums[left] == target

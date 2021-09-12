45e1b0e35fed398db4a58b7ed326f420d5ec5bc0
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-09-12 21:47:58 +0800

    Time: 37 ms (7.20%), Space: 13.6 MB (26.46%) - LeetHub

--- binary-tree-right-side-view/binary-tree-right-side-view.py
+++ binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,29 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def rightSideView(self, root: TreeNode) -> List[int]:
-        ans = []
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if root is None: return []
         
-        if (root is None): return ans
-        if (root.right is None and root.left is None): return [root.val]
+        self.height = 0
+        levels = [None]*101 # 101 because 100 is the max nodes in the tree
         
-        queue = []
-        queue.append(root)
-        
-        while queue:
-            s = len(queue)
-            for i in range(s):
-                curr = queue[0]
-                queue.pop(0)
-                if (i == s-1):
-                    ans.append(curr.val)
-                if (curr.left):
-                    queue.append(curr.left)
-                if (curr.right):
-                    queue.append(curr.right)
-        return ans
+        def recurse(node = root, level = 0):
+            if node is None: return
+            self.height = max(self.height, level)
             
+            if levels[level] is None: levels[level] = node.val
+                
+            recurse(node.right, level + 1)
+            recurse(node.left, level + 1)
+        
+        recurse()
+        return levels[:self.height + 1]

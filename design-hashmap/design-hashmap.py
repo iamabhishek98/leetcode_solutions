9cde39ccf68af843ccd1e585fa4ae4c258a217ab
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-09-13 21:44:42 +0800

    Time: 2640 ms (8.28%), Space: 18.1 MB (36.45%) - LeetHub

--- design-hashmap/design-hashmap.py
+++ design-hashmap/design-hashmap.py
@@ -1,10 +1,17 @@
-class MyHashMap(object):
+class ListNode(object):
+    def __init__(self, key = None, val = None, next = None):
+        self.key = key
+        self.val = val
+        self.next = next
 
+class MyHashMap(object):
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.value = [-1]*1000000
+        self.BUCKET_SIZE = 100
+        
+        self.buckets = [ListNode() for _ in range(self.BUCKET_SIZE)]
         
 
     def put(self, key, value):
@@ -14,7 +21,22 @@ class MyHashMap(object):
         :type value: int
         :rtype: None
         """
-        self.value[key] = value
+        index = key % self.BUCKET_SIZE
+        
+        head = self.buckets[index]
+        
+        while head.next:
+            if head.key == key: 
+                head.val = value
+                return
+            head = head.next
+        
+        if head.key == key: 
+            head.val = value
+            return
+        
+        head.next = ListNode(key, value)
+        
 
     def get(self, key):
         """
@@ -22,15 +44,40 @@ class MyHashMap(object):
         :type key: int
         :rtype: int
         """
-        return self.value[key]
+        index = key % self.BUCKET_SIZE
         
+        head = self.buckets[index]
+        
+        while head:
+            if head.key == key:
+                return head.val
+            head = head.next
+        
+        return -1
+
     def remove(self, key):
         """
         Removes the mapping of the specified value key if this map contains a mapping for the key
         :type key: int
         :rtype: None
         """
-        self.value[key] = -1
+        index = key % self.BUCKET_SIZE
+        
+        head = self.buckets[index]
+        
+        while head:
+            if head.key == key:
+                if head.next:
+                    head.key = head.next.key
+                    head.val = head.next.val
+                    head.next = head.next.next
+                else:
+                    head.key = None
+                    head.val = None
+                    head.next = None
+                return
+            head = head.next
+        
 
 
 # Your MyHashMap object will be instantiated and called as such:

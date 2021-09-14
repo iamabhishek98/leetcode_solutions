c19c7ff906a6b5ed3e3c927ce7c1507865afe57d
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-09-14 11:22:33 +0800

    Time: 4116 ms (5.08%), Space: 157.8 MB (5.08%) - LeetHub

--- longest-common-subsequence/longest-common-subsequence.py
+++ longest-common-subsequence/longest-common-subsequence.py
@@ -5,12 +5,21 @@ class Solution(object):
         :type text2: str
         :rtype: int
         """
-        self.dp = [[-1 for i in range(len(text2))] for j in range(len(text1))]
-        def lcs(i,j):
+        # https://www.youtube.com/watch?v=sSno9rV8Rhg&ab_channel=AbdulBari
+        self.dp = {}
+        def lcs(i = 0, j = 0):
             if i >= len(text1) or j >= len(text2): return 0
-            if self.dp[i][j] != -1: return self.dp[i][j]
-            if text1[i] == text2[j]: return 1 + lcs(i+1,j+1)
-            self.dp[i][j] = max(lcs(i+1,j),lcs(i,j+1))
-            return self.dp[i][j]
-        return lcs(0,0)
+            
+            key = tuple([i,j])
+            
+            if key in self.dp: return self.dp[key]
+            
+            if text1[i] == text2[j]: 
+                return 1 + lcs(i+1,j+1)
+            
+            self.dp[key] = max(lcs(i+1,j), lcs(i,j+1))
+            
+            return self.dp[key]
+        
+        return lcs()
         

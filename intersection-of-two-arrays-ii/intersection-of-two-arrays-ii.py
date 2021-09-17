77c8f4dbdc9b02de403758853b30685c6a9652b1
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-09-18 06:10:05 +0800

    Time: 28 ms (95.46%), Space: 13.6 MB (61.81%) - LeetHub

--- intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -7,12 +7,47 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
+        # Method 1
+        
         c1 = Counter(nums1)
-        c2 = Counter(nums2)
         
         intersection = []
         
-        for k1 in c1:
-            intersection.extend(min(c1[k1],c2.get(k1,0))*[k1])
+        for ele in nums2:
+            if c1.get(ele, 0) > 0:
+                c1[ele] -= 1
+                intersection.append(ele)
         
         return intersection
+                
+#         # Method 2
+#         c1 = Counter(nums1)
+#         c2 = Counter(nums2)
+        
+#         intersection = []
+        
+#         for k1 in c1:
+#             intersection.extend(min(c1[k1],c2.get(k1,0))*[k1])
+        
+#         return intersection
+
+
+#         # Method 3
+    
+#         s1 = set()
+#         s2 = set()
+#         m1 = {}
+#         m2 = {}
+#         for i in nums1:
+#             s1.add(i)
+#             if (i in m1): m1[i]+=1
+#             else: m1[i]=1
+#         for i in nums2:
+#             s2.add(i)
+#             if (i in m2): m2[i]+=1
+#             else: m2[i]=1
+#         inter = (list(s1 & s2))
+#         ans = []
+#         for i in inter:
+#             ans.extend([i]*min(m1[i],m2[i]))
+#         return ans

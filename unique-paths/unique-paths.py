f7293b851a5999355eca1348143243bb770cfae2
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-10-02 13:46:13 +0800

    Time: 24 ms (36.50%), Space: 13.5 MB (21.39%) - LeetHub

--- unique-paths/unique-paths.py
+++ unique-paths/unique-paths.py
@@ -5,19 +5,33 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        memo = {}
+        # Method 1: Top down DP approach
+        dp = [[0 for _ in range(n+1)] for __ in range(m+1)]
+        dp[1][1] = 1
         
-        def recurse(m = m, n = n):
-            # base case: reached finish cell
-            if m == 1 and n == 1: return 1
-            # base case: not possible
-            if m == 0 or n == 0: return 0
-            
-            key = str(m)+":"+str(n)
-            if key in memo: return memo[key]
-            
-            # travel down or to the right
-            memo[key] = recurse(m-1,n) + recurse(m,n-1) 
-            return memo[key]
+        for i in range(m+1):
+            for j in range(n+1):
+                # add to bottom cell
+                if i+1 <= m: dp[i+1][j] += dp[i][j]
+                # add to right cell
+                if j+1 <= n: dp[i][j+1] += dp[i][j]
         
-        return recurse()
+        return dp[m][n]
+        
+#         # Method 2: Memoization (Bottom up DP approach)
+#         memo = {}
+        
+#         def recurse(m = m, n = n):
+#             # base case: reached finish cell
+#             if m == 1 and n == 1: return 1
+#             # base case: not possible
+#             if m == 0 or n == 0: return 0
+            
+#             key = str(m)+":"+str(n)
+#             if key in memo: return memo[key]
+            
+#             # travel down or to the right
+#             memo[key] = recurse(m-1,n) + recurse(m,n-1) 
+#             return memo[key]
+        
+#         return recurse()

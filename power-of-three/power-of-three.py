fb3ea1d00e728a94152d4f3eaa28780c3304d789
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-10-07 10:34:44 +0800

    Time: 100 ms (37.56%), Space: 13.3 MB (89.63%) - LeetHub

--- power-of-three/power-of-three.py
+++ power-of-three/power-of-three.py
@@ -4,11 +4,15 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
+        # method 1: divide repeatedly
         ori = n
         if (n<=0): return False
         i = 0
+        curr = 1
         while(n>0):
             n/=3
+            if i != 0: curr *= 3
             i+=1
-        if (pow(3,i-1)==ori): return True
+            
+        if (curr==ori): return True
         return False

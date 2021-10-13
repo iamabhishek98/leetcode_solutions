3e9c6367cdb3294dd5917c8da971dbb2a2cf25db
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-10-13 12:01:59 +0800

    Time: 28 ms (39.65%), Space: 13.6 MB (11.37%) - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -4,14 +4,27 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n == 0 or n == 1: return n
-        a = 0 
-        b = 1
-        c = 0
+        # top down memoization
+        memo = {}
+        def recurse(i = n):
+            if i == 0 or i == 1: return i
+            if i in memo: return memo[i]
+            
+            memo[i] = recurse(i-1) + recurse(i-2)
+            
+            return memo[i]
+            
+        return recurse()
         
-        for i in range(2, n + 1):
-            c = a + b
-            a, b = b, c
+#         # bottom up optimized tabulation
+#         if n == 0 or n == 1: return n
+#         a = 0 
+#         b = 1
+#         c = 0
         
-        return c
+#         for i in range(2, n + 1):
+#             c = a + b
+#             a, b = b, c
+        
+#         return c
         

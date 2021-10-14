9352c0b1d09cc40d0c2435c98b9183ee76678309
Karnati Sai Abhishek <42940729+iamabhishek98@users.noreply.github.com>
2021-10-15 06:13:26 +0800

    Time: 92 ms (55.57%), Space: 16.9 MB (53.94%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -9,13 +9,28 @@ class Solution(object):
         """
         c = Counter(nums)
         
-        max_pq = []
+        # method 1: min heap
+        min_heap = []
+        heapq.heapify(min_heap)
         for key in c:
-            heapq.heappush(max_pq, tuple([-c[key],key]))
+            heapq.heappush(min_heap, [c[key],key])
+            if len(min_heap) > k: heapq.heappop(min_heap)
         
         ans = []
-        for i in range(k):
-            ans.append(heapq.heappop(max_pq)[1])
-            
+        while min_heap:
+            ans.append(heapq.heappop(min_heap)[1])
         return ans
+        
+#         # method 2: max heap        
+#         max_heap = []
+#         heapq.heapify(max_heap)
+
+#         for key in c:
+#             heapq.heappush(max_heap, tuple([-c[key],key]))
+        
+#         ans = []
+#         for i in range(k):
+#             ans.append(heapq.heappop(max_heap)[1])
+            
+#         return ans
         
